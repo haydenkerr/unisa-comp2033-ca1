65f2fcff040ab51236baab125b5e9c5ab5ca1fee
haydenSyncezy <hayden@syncezy.com>
2023-04-13 06:43:56 +1000

    created

--- CA1/Continuous Assessment 1/src/ca1/task4/Car.java
+++ CA1/Continuous Assessment 1/src/ca1/task4/Car.java
@@ -1,4 +1,20 @@
 package ca1.task4;
+/*
+ * Task 4: “Remove duplicate object from array”
+
+This task requires you to implement the equals() and hashCode() methods in the Car class. 
+Two cars are equal if they have the same model and colour, regardless of make.
+
+Implement the ‘toString()’ method of the Car class. 
+The toString() should return a string representation of a Car. 
+The format should be “<Make>, <Model>, <Color>”.   
+Implement the ‘removeDuplicates’ method in the CarProcessing class. 
+Your method should remove duplicates from the given array using the Car equals() method. 
+Write unit tests for both the Car and the CarProcessing class. 
+
+
+ */
+
 
 public class Car {
 	private String make;
