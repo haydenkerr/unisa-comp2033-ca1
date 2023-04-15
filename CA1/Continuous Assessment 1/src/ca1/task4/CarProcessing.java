002d714293b3e9a10b4d13388e50024a01b8e68c
Hayden Kerr <kerhy006@mymail.unisa.edu.au>
2023-04-16 05:30:55 +1000

    updated removeDuplicates

--- CA1/Continuous Assessment 1/src/ca1/task4/CarProcessing.java
+++ CA1/Continuous Assessment 1/src/ca1/task4/CarProcessing.java
@@ -1,5 +1,11 @@
 package ca1.task4;
 
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import ca1.task4.Car;
+
 public class CarProcessing {
 	public static void main(String[] args) {
 		
@@ -22,8 +28,21 @@ public class CarProcessing {
 			System.out.println(c);
 		}
 	}
+	// https://javarevisited.blogspot.com/2015/06/3-ways-to-find-duplicate-elements-in-array-java.html#:~:text=The%20standard%20way%20to%20find,property%20to%20filter%20duplicate%20elements.
+	
 	
 	public Car[] removeDuplicates(Car[] inputArray) {
+		
+	      System.out.println("Duplicate elements from array using HashSet data structure");
+	      Set<Car> set = new HashSet<>();
+
+	      for (Car car : cars) {
+	            if (set.equals() == false) {
+	                System.out.println("found a duplicate element in array : "
+	                        + name);
+	            }
+	        }
+
 		return null;
 	}
 	
