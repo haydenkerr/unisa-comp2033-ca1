e335658c7df91a226c18c3b09015af8e3ee80f31
Hayden Kerr <kerhy006@mymail.unisa.edu.au>
2023-04-23 10:46:36 +1000

    variation of jUnit tests.
    Completed and consistent.
    
    one, two, three counts confirmed

--- CA1/Continuous Assessment 1/src/ca1/task4/CarProcessingTest.java
+++ CA1/Continuous Assessment 1/src/ca1/task4/CarProcessingTest.java
@@ -1,5 +1,85 @@
 package ca1.task4;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class CarProcessingTest {
 
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testDuplicateOne() {
+		CarProcessing cp = new CarProcessing();
+	
+		
+		Car[] cars = { 
+				new Car("Honda", "Civic", "Red"),
+				new Car("Holden", "Civic", "Red"),
+	
+		};
+		
+		Car[] noDuplicatesObj = cp.removeDuplicates(cars);
+		
+	
+		
+		int expectedCount = 1;
+		
+
+		assertEquals(expectedCount, noDuplicatesObj.length, "The count does not work as expected.");
+	}
+
+	@Test
+	void testDuplicateTwo() {
+	CarProcessing cp = new CarProcessing();
+	
+	
+	Car[] cars = { 
+			new Car("Honda", "Civic", "Red"),
+			new Car("Holden", "Civic", "Red"),
+			new Car("Holden", "Rodeo", "Red")
+
+	};
+	
+	Car[] noDuplicatesObj = cp.removeDuplicates(cars);
+	
+
+	
+	int expectedCount = 2;
+	
+
+	assertEquals(expectedCount, noDuplicatesObj.length, "The count does not work as expected.");
 }
+
+	@Test
+	void testDuplicateThree() {
+	CarProcessing cp = new CarProcessing();
+	
+	
+	Car[] cars = { 
+			new Car("Honda", "Civic", "Red"),
+			new Car("Tesla", "Y", "Red"),
+			new Car("Holden", "Rodeo", "Red")
+
+	};
+	
+	Car[] noDuplicatesObj = cp.removeDuplicates(cars);
+	for(int i =0; i<noDuplicatesObj.length; i++)
+		System.out.println(noDuplicatesObj[i]);
+
+	
+	int expectedCount = 3;
+	
+
+	assertEquals(expectedCount, noDuplicatesObj.length, "The count does not work as expected.");
+	
+}
+}
+
+	// 
+
